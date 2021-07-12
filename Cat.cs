0a81bb33b7b2ec94381b26f1a0972dcb4d965b5b
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-07-12 19:15:12 +0300

    add Project Files

---
+++ Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood)
+            : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine +"MEEOW";
+        }
+    } //Polymorphism is the ability of an object to take on many forms
+}
